4882ed0c8f47bfaf099c92732b3bfccf73247f16
mpcny <system@mpcny.org>
2020-10-01 17:18:46 -0400

    trying

---
+++ deplpy.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo -t educenter-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site mpcny.org $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+cd ..
+hugo -t educenter-hugo --config config_mariepensecenter
+cd public_mariepensecenter
+git add .
+
+# Commit changes.
+msg="rebuilding site mariepensecenter.com $(date)"
+if [ -n "$*" ]; then
+	  msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push mpcny master
